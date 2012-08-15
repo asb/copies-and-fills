80f6ed71222acd1e51bd888d80b4c68d4a04e627
Alex Bradbury <asb@asbradbury.org>
2012-08-16 00:02:52 +0100

    bump version and fix up permissions

--- makedeb.sh
+++ makedeb.sh
@@ -4,7 +4,10 @@ set -ex
 
 rm -rf dist
 make
+fakeroot sh - <<EOF
 mkdir -p dist/usr/lib/arm-linux-gnueabihf
 cp -r DEBIAN dist
 cp libcofi_rpi.so dist/usr/lib/arm-linux-gnueabihf
+chown root:root -R dist
 dpkg -b dist raspi-copies-and-fills_0.1-armhf.deb
+EOF
