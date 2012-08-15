51a43f0612926887d33095d7a953ed0e7ec115cc
Alex Bradbury <asb@asbradbury.org>
2012-08-16 00:04:02 +0100

    fix version for generated deb

--- makedeb.sh
+++ makedeb.sh
@@ -9,5 +9,5 @@ mkdir -p dist/usr/lib/arm-linux-gnueabihf
 cp -r DEBIAN dist
 cp libcofi_rpi.so dist/usr/lib/arm-linux-gnueabihf
 chown root:root -R dist
-dpkg -b dist raspi-copies-and-fills_0.1-armhf.deb
+dpkg -b dist raspi-copies-and-fills_0.2-armhf.deb
 EOF
